9df9d08b0a63d88df65b1ca75271fca0f95788e8
DivyangHirpara <divyanghirpara5@gmail.com>
2024-04-09 15:26:21 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace az204_hello.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
